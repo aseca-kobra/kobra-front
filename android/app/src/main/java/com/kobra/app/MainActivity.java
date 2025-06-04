71330bf01608016a5e1797e418ddd38d87dce7d5
pedro <pedroramirezneira@gmail.com>
2025-06-04 14:17:43 -0300

    feat: enable web contents debugging

--- android/app/src/main/java/com/kobra/app/MainActivity.java
+++ android/app/src/main/java/com/kobra/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.kobra.app;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -11,5 +13,6 @@ public class MainActivity extends BridgeActivity {
         var webView = this.bridge.getWebView();
         var settings = webView.getSettings();
         settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        WebView.setWebContentsDebuggingEnabled(true);
     }
 }
