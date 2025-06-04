eaf1b2f97a2179846b7b04eaa03062911494a9ba
pedro <pedroramirezneira@gmail.com>
2025-06-04 16:15:57 -0300

    feat: add input validation tests

--- appium/test/specs/test.e2e.ts
+++ appium/test/specs/test.e2e.ts
@@ -1,14 +1,56 @@
 import { expect, browser, $ } from "@wdio/globals";
 
-describe("Invalid Login Email", () => {
-  it("should not login with invalid email", async () => {
+describe("Invalid Email", () => {
+  it("should not accept invalid email", async () => {
     await browser.pause(2000);
     const contexts = await browser.getContexts();
     const webviewContext = contexts.find((c) => c.includes("WEBVIEW"));
     expect(webviewContext).toBeDefined();
     await browser.switchContext(webviewContext!);
-    await $("#email").setValue("pedro@hotmail.com");
+    await $("#email").setValue("pedro");
     await $("#password").setValue("pedro123");
+    await $('button[type="submit"]').click();
+    await browser.pause(2000);
+    const errorHelperText = await $("#email-helper-text");
+    await expect(errorHelperText).toBeDisplayed();
+    await expect(errorHelperText).toHaveText("Email inválido");
+  });
+});
+
+describe("Invalid Password", () => {
+  it("should not accept invalid password", async () => {
+    const emailInput = await $("#email");
+    await emailInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await emailInput.setValue("pedro@gmail.com");
+    const passwordInput = await $("#password");
+    await passwordInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await passwordInput.setValue("p");
+    await $('button[type="submit"]').click();
+    await browser.pause(2000);
+    const errorHelperText = await $("#password-helper-text");
+    await expect(errorHelperText).toBeDisplayed();
+    await expect(errorHelperText).toHaveText(
+      "La contraseña debe tener al menos 6 caracteres"
+    );
+  });
+});
+
+describe("Invalid Login Email", () => {
+  it("should not login with invalid email", async () => {
+    const emailInput = await $("#email");
+    await emailInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await emailInput.setValue("pedro@hotmail.com");
+    const passwordInput = await $("#password");
+    await passwordInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await passwordInput.setValue("pedro123");
     await $('button[type="submit"]').click();
     await browser.pause(2000);
     const errorMessage = await $(".MuiAlert-message");
