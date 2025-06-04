be30161ab1574848f5ea7862461457acdfa43615
Santos <santosbogo@gmail.com>
2025-06-04 17:30:53 -0300

    add react name labels to log out message buttons

--- src/components/wallet/HeaderBar.tsx
+++ src/components/wallet/HeaderBar.tsx
@@ -48,10 +48,11 @@ const HeaderBar = () => {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleCloseDialog} color="primary">
+                    <Button onClick={handleCloseDialog} name={"cancel"} color="primary">
                         Cancelar
                     </Button>
                     <Button
+                        name={"confirm"}
                         onClick={() => {
                             handleCloseDialog();
                             logout();
