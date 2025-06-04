0b26a5489aeefb67c708b23261a098627c80090d
pedro <pedroramirezneira@gmail.com>
2025-06-04 16:08:24 -0300

    feat: initialize appium, implement login tests

---
+++ appium/test/specs/test.e2e.ts
@@ -0,0 +1,57 @@
+import { expect, browser, $ } from "@wdio/globals";
+
+describe("Invalid Login Email", () => {
+  it("should not login with invalid email", async () => {
+    await browser.pause(2000);
+    const contexts = await browser.getContexts();
+    const webviewContext = contexts.find((c) => c.includes("WEBVIEW"));
+    expect(webviewContext).toBeDefined();
+    await browser.switchContext(webviewContext!);
+    await $("#email").setValue("pedro@hotmail.com");
+    await $("#password").setValue("pedro123");
+    await $('button[type="submit"]').click();
+    await browser.pause(2000);
+    const errorMessage = await $(".MuiAlert-message");
+    await expect(errorMessage).toBeDisplayed();
+    await expect(errorMessage).toHaveText("Invalid credentials");
+  });
+});
+
+describe("Invalid Login Password", () => {
+  it("should not login with invalid password", async () => {
+    const emailInput = await $("#email");
+    await emailInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await emailInput.setValue("pedro@gmail.com");
+    const passwordInput = await $("#password");
+    await passwordInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await passwordInput.setValue("pedro456");
+    await $('button[type="submit"]').click();
+    await browser.pause(2000);
+    const errorMessage = await $(".MuiAlert-message");
+    await expect(errorMessage).toBeDisplayed();
+    await expect(errorMessage).toHaveText("Invalid credentials");
+  });
+});
+
+describe("Valid Login", () => {
+  it("should login with valid credentials", async () => {
+    const emailInput = await $("#email");
+    await emailInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await emailInput.setValue("pedro@gmail.com");
+    const passwordInput = await $("#password");
+    await passwordInput.click();
+    await browser.keys(["Control", "a"]); // Ctrl+A (para seleccionar todo)
+    await browser.keys("Delete"); // Borrar
+    await passwordInput.setValue("pedro123");
+    await $('button[type="submit"]').click();
+    await browser.pause(2000);
+    const currentUrl = await browser.getUrl();
+    expect(currentUrl).toContain("/home");
+  });
+});
