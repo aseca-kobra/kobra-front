1f56576609bea1f544b5699ce182b7845bac0a24
pedro <pedroramirezneira@gmail.com>
2025-06-04 12:36:08 -0300

    feat: initialize capacitor with android and ios platforms

---
+++ android/app/src/main/java/com/kobra/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kobra.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        var webView = this.bridge.getWebView();
+        var settings = webView.getSettings();
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+    }
+}
