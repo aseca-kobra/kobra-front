09a0774d485df51ab45a45d7324ecd092a98b08d
Tomas Valle Duran <tvalleduran@mail.austral.edu.com>
2025-06-05 23:16:36 -0300

    Uncommented some tests

--- cypress/e2e/transfer.cy.ts
+++ cypress/e2e/transfer.cy.ts
@@ -21,48 +21,48 @@ describe("Transfer", () => {
         cy.get('button[name="transfer"]').click();
     });
 
-    // it("Invalid transfer (empty email)", () => {
-    //     cy.get('button[name="confirm"]').click();
-    //     cy.contains("El email es requerido");
-    //     cy.get('button[name="cancel"]').click();
-    //
-    //     getBalance().should("equal", initialBalance);
-    // });
-    //
-    // it("Invalid transfer (invalid email format)", () => {
-    //     cy.get('input[name="email"]').type("incorrectemailformat");
-    //     cy.get('button[name="confirm"]').click();
-    //     cy.contains("Ingrese un email válido");
-    //     cy.get('button[name="cancel"]').click();
-    //
-    //     getBalance().should("equal", initialBalance);
-    // });
-    //
-    // it("Invalid transfer (empty amount)", () => {
-    //     cy.get('button[name="confirm"]').click();
-    //     cy.contains("El monto es requerido");
-    //     cy.get('button[name="cancel"]').click();
-    //
-    //     getBalance().should("equal", initialBalance);
-    // });
-    //
-    // it("Invalid transfer (amount equals 0)", () => {
-    //     cy.get('input[name="amount"]').type("0");
-    //     cy.get('button[name="confirm"]').click();
-    //     cy.contains("Ingrese un monto válido mayor a 0");
-    //     cy.get('button[name="cancel"]').click();
-    //
-    //     getBalance().should("equal", initialBalance);
-    // });
-    //
-    // it("Invalid transfer (not enough balance)", () => {
-    //     cy.get('input[name="amount"]').type("10000000000000");
-    //     cy.get('button[name="confirm"]').click();
-    //     cy.contains("Saldo insuficiente para realizar la transferencia");
-    //     cy.get('button[name="cancel"]').click();
-    //
-    //     getBalance().should("equal", initialBalance);
-    // });
+    it("Invalid transfer (empty email)", () => {
+        cy.get('button[name="confirm"]').click();
+        cy.contains("El email es requerido");
+        cy.get('button[name="cancel"]').click();
+
+        getBalance().should("equal", initialBalance);
+    });
+
+    it("Invalid transfer (invalid email format)", () => {
+        cy.get('input[name="email"]').type("incorrectemailformat");
+        cy.get('button[name="confirm"]').click();
+        cy.contains("Ingrese un email válido");
+        cy.get('button[name="cancel"]').click();
+
+        getBalance().should("equal", initialBalance);
+    });
+
+    it("Invalid transfer (empty amount)", () => {
+        cy.get('button[name="confirm"]').click();
+        cy.contains("El monto es requerido");
+        cy.get('button[name="cancel"]').click();
+
+        getBalance().should("equal", initialBalance);
+    });
+
+    it("Invalid transfer (amount equals 0)", () => {
+        cy.get('input[name="amount"]').type("0");
+        cy.get('button[name="confirm"]').click();
+        cy.contains("Ingrese un monto válido mayor a 0");
+        cy.get('button[name="cancel"]').click();
+
+        getBalance().should("equal", initialBalance);
+    });
+
+    it("Invalid transfer (not enough balance)", () => {
+        cy.get('input[name="amount"]').type("10000000000000");
+        cy.get('button[name="confirm"]').click();
+        cy.contains("Saldo insuficiente para realizar la transferencia");
+        cy.get('button[name="cancel"]').click();
+
+        getBalance().should("equal", initialBalance);
+    });
 
     it("Successful transfer", () => {
         const amount = 10;
